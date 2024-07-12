0005878bc33af562e0bb0be507e819d43a5f0d26
royali24 <royali.roy24@gmail.com>
2024-07-11 21:38:38 -0400

    Set and Sort Array method

---
+++ src/collectionsJava/CreateSet.java
@@ -0,0 +1,44 @@
+package collectionsJava;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CreateSet {
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<String> travelwish = new HashSet();
+		travelwish.add("Venice");
+		travelwish.add("Tuscany");
+		travelwish.add("Edinburg");
+		travelwish.add("Amsterdam");
+		travelwish.add("Normandy");
+		travelwish.add("Paris");
+		
+		System.out.println("Travel list city:  " + travelwish);
+		
+		//Remove a city
+		travelwish.remove("Normandy");
+		System.out.println("Updated Travel list city:  " + travelwish);
+		
+		//iterate through all cities to find an element
+		iterateSetValues(travelwish);
+	}
+
+	private static void iterateSetValues(Set<String> travelwish) {
+		// TODO Auto-generated method stub
+		System.out.println("Iterating through the travel wish list");
+		for(String s : travelwish) {
+			
+			//System.out.println(s);
+			
+			if(s.equalsIgnoreCase("Paris")) {
+				System.out.println("Found my favourite city : " + s);
+			}
+		}
+	}
+
+}
---
+++ src/programsJava/SortArray.java
@@ -0,0 +1,65 @@
+package programsJava;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = {6,5,8,3,4,2};
+		int len = arr.length;
+		System.out.println("Size of array: " + len);
+		
+		
+		sortArrayLowesttoHighest(arr);
+		System.out.println("\n");
+		sortArrayHighesttoLowest(arr);
+		
+	}
+
+	private static void sortArrayHighesttoLowest(int[] array) {
+		int len = array.length;
+		System.out.print("{");  
+		int temp = 0;
+		for( int i=0; i<len; i++){
+			for (int j= i+1; j<len; j++){
+
+				if(array[i]<array[j]){
+
+					temp= array[i];
+					array[i]=array[j];
+					array[j]= temp;
+				}
+			}
+			System.out.print(array[i]);  
+			if(i<len-1)
+				System.out.print(", ");  
+		}
+		System.out.print("}");  
+		
+	}
+		
+	
+
+	private static void sortArrayLowesttoHighest(int[] array) {
+		int len = array.length;
+		System.out.print("{");  
+		int temp = 0;
+		for( int i=0; i<len; i++){
+			for (int j= i+1; j<len; j++){
+
+				if(array[i]>array[j]){
+
+					temp= array[i];
+					array[i]=array[j];
+					array[j]= temp;
+				}
+			}
+			System.out.print(array[i]);  
+			if(i<len-1)
+				System.out.print(", ");  
+		}
+		System.out.print("}");  
+		
+	}
+
+}
